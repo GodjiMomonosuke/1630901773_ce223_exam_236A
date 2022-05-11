70a267c4f31d310e7bbbfd89e53cfc427cadc2db
Tivanaht04 <tthanasiriwerachot@gmail.com>
2022-05-11 14:13:19 +0700

    ejs

--- nodejs_sql.js
+++ nodejs_sql.js
@@ -17,7 +17,15 @@ const pool = mysql.createPool({
     password : '',
     database : 'lotto' 
 })
- 
+
+//view
+app.set('view engine','ejs')
+
+var obj = {}
+
+app.get('/addnumber',(req, res) => {   
+    res.render('addnumber')
+})
 
 app.get('',(req, res) => {
  
@@ -30,7 +38,10 @@ app.get('',(req, res) => {
             if(!err){ 
                 //console.log(rows)
                 //res.json(rows)
-                res.send(rows)
+                
+                obj = { lottery : rows, Error : err}
+            
+                res.render('index', obj)
             } else {
                 console.log(err)
             }
@@ -48,7 +59,11 @@ app.get('/:id',(req, res) => {
         connection.query('SELECT * FROM lottery WHERE `id` = ?', req.params.id, (err, rows) => { 
             connection.release();
             if(!err){ //ถ้าไม่ error จะใส่ในตัวแปร rows
-                res.send(rows)
+                //res.send(rows)
+                obj = {lottery : rows, Error, err}               
+                res.render('showbyid', obj)
+
+
             } else {
                 console.log(err)
             }
@@ -64,14 +79,29 @@ app.post('/addnumber',(req, res) => {
     pool.getConnection((err, connection) => { //pool.getConnection สำหรับใช้เชื่อมต่อกับ Database 
         if(err) throw err
             const params = req.body
-                connection.query('INSERT INTO lottery SET ?', params, (err, rows) => {
-                    connection.release()
-                    if(!err){
-                        res.send(`${params.number} is complete adding item. `)
-                    }else {
-                        console.log(err)
-                        }
-                    })           
+
+            
+                //Check 
+                pool.getConnection((err, connection2) => {
+                    connection2.query(`SELECT COUNT(id) AS count FROM lottery WHERE id = ${params.id}`, (err, rows) => {
+                        if(!rows[0].count){
+                            connection.query('INSERT INTO lottery SET ?', params, (err, rows) => {
+                                connection.release()
+                                if(!err){
+                                    //res.send(`${params.number} is complete adding number. `)
+                                    obj = {Error:err, mesg : `Success adding data ${params.number}`}                                   
+                                    res.render('addnumber', obj)
+                                }else {
+                                    console.log(err)
+                                    }
+                                })           
+                        } else {
+                            //res.send(`${params.number} do not insert data`)
+                            obj = {Error:err, mesg : `Can not adding data ${params.number}`}                           
+                            res.render('addnumber', obj)
+                            }
+                        })
+                    })
                 })
             })
 
@@ -91,7 +121,28 @@ app.delete('/delete/:id',(req, res) => {
         })
     })
 })
+
+//(3)PUT ทำการ UPDATE ข้อมูลใน Database ใช้วิธีการทดสอบทำเช่นเดียวกับของ POST
+app.put('/update',(req, res) => {
+    pool.getConnection((err, connection) =>{
+        if(err) throw err
+        console.log("connected id : ?", connection.threadId)
  
+        //สร้างตัวแปรแบบกลุ่ม
+        const {id, date, type, number, reward} = req.body       
+ 
+        //Update ข้อมูลต่าง ๆ ตามลำดับ โดยใช้เงื่อนไข id
+        connection.query('UPDATE lottery SET date = ?, type = ?, number = ?, reward = ? WHERE id = ?', [date, type, number, reward, id], (err, rows) => {
+            connection.release();
+            if(!err){ 
+                res.send(`${number} is complete update number. `) 
+            } else {
+                console.log(err)
+            }
+        })
+    })
+})
+
 app.listen(port, () => 
     console.log("listen on port : ?", port)
     )
